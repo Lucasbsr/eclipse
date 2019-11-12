8bc725295dee4d3769d73f93b12856ce621c6b1c
Lucasbsr <57644930+Lucasbsr@users.noreply.github.com>
2019-11-11 21:27:31 -0300

    qualquer coisa

---
+++ helloGitHub/src/helloGitHub/hellougit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package helloGitHub;
+
+/**
+ * @author lucas
+ *
+ */
+public class hellougit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ helloGitHub/src/module-info.java
@@ -0,0 +1,2 @@
+module helloGitHub {
+}
